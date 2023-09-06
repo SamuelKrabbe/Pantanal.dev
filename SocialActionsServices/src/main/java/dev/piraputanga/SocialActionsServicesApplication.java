b233445df9831801ed7e4de5391b195715441c4f
PacMan111 <luizgustavossj@gmail.com>
2023-09-05 20:05:39 -0400

    feat: start project

---
+++ SocialActionsServices/src/main/java/dev/piraputanga/SocialActionsServicesApplication.java
@@ -0,0 +1,13 @@
+package dev.piraputanga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialActionsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialActionsServicesApplication.class, args);
+	}
+
+}
---
+++ SocialActionsServices/src/test/java/dev/piraputanga/SocialActionsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.piraputanga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialActionsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
