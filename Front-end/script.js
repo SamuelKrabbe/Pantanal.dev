f47b42f0d7b8dc4ab30a805900db63a5ceb42ac4
SamuelKrabbe <samuelkrabbe@hotmail.com>
2023-09-24 21:56:55 -0400

    feat: página para ver lista de ações criada, bug para pegar a resposta do back-end

--- Front-end/script.js
+++ Front-end/script.js
@@ -1,3 +1,4 @@
+// FORMS DO CADASTRO DE AÇÕES
 function openModal() {
 	var modal = document.getElementById("myModal");
 	modal.style.display = "block";
@@ -8,6 +9,7 @@ function closeModal() {
 	modal.style.display = "none";
 }
 
+// BARRA DE PESQUISA
 function toggleSearch() {
 	var searchBox = document.getElementById("searchBox");
 	var openSearchBox = document.getElementById("openSearchBox");
@@ -21,4 +23,65 @@ function toggleSearch() {
 		openSearchBox.style.display = "none";
 		closeSearchBox.style.display = "flex";
 	}
+}
+
+// PÁGINA DA LISTA DE AÇÕES SOCIAIS
+document
+	.getElementById("fetchSocialActions")
+	.addEventListener("click", openSocialActionsListPage);
+
+function openSocialActionsListPage() {
+	var socialActionsList = document.getElementById("socialActionsList");
+	var container = document.getElementById("container");
+	socialActionsList.style.display = "flex";
+	container.style.display = "none";
+}
+
+document
+	.getElementById("backToMainPage")
+	.addEventListener("click", closeSocialActionsListPage);
+
+function closeSocialActionsListPage() {
+	var socialActionsList = document.getElementById("socialActionsList");
+	var container = document.getElementById("container");
+	socialActionsList.style.display = "none";
+	container.style.display = "block";
+}
+
+// PEGA TODAS AS AÇÕES SOCIAIS
+document
+	.getElementById("fetchSocialActions")
+	.addEventListener("click", getSocialActions);
+
+function getSocialActions() {
+	const url = "http://localhost:8080/socialactions";
+
+	const headers = new Headers();
+	headers.append("Content-Type", "application/json"); // Example header, replace with your headers
+
+	const requestOptions = {
+		method: "GET", // HTTP method (GET, POST, PUT, DELETE, etc.)
+		headers: headers,
+	};
+
+	fetch(url, requestOptions)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+			return response;
+		})
+		.then((data) => {
+			for (const item in data) {
+				console.log(item);
+				document
+					.createElement("ul")
+					.appendChild(
+						document
+							.createElement("li")
+							.appendChild(document.createTextNode(item.id))
+					);
+			}
+		})
+		.catch((error) => console.error("Error fetching data:", error));
 }
