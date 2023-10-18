4ab70f801e4e08deeebf94e6a115dcea75dbbcfc
PacMan111 <luizgustavossj@gmail.com>
2023-10-17 22:46:01 -0400

    chore: remove unnecessary service

--- Back-end/SocialActions/src/main/java/dev/piraputanga/socialaction/UserSignUpRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.piraputanga.socialaction;
-
-import java.util.List;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface UserSignUpRepository extends ListCrudRepository<UserSignUp, String> {
-    public List<UserSignUp> findByUsernameContainingIgnoreCase(String texto);
-}
