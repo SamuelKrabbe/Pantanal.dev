b5533e03584350b8ae281a25e78631d4ed80ae45
PacMan111 <luizgustavossj@gmail.com>
2023-10-25 22:34:28 -0400

    fix: missing import on social action service

--- Back-end/SocialActions/src/main/java/dev/piraputanga/service/SocialActionService.java
+++ Back-end/SocialActions/src/main/java/dev/piraputanga/service/SocialActionService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.stereotype.Service;
 import org.springframework.http.HttpStatusCode;
