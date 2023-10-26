cb5f233e1a7513197031a1b69c48da04e51e32b5
PacMan111 <luizgustavossj@gmail.com>
2023-10-26 19:17:08 -0400

    fix(DuplicateKey): fix social contract duplicate key condition

--- Back-end/SocialContracts/src/main/java/dev/piraputanga/repository/SocialContractRepository.java
+++ Back-end/SocialContracts/src/main/java/dev/piraputanga/repository/SocialContractRepository.java
@@ -9,4 +9,6 @@ import dev.piraputanga.model.SocialContract;
 public interface SocialContractRepository extends ListCrudRepository<SocialContract, Long> {
     public List<SocialContract> findByUserEmailContainingIgnoreCase(String texto);
     public List<SocialContract> findBySocialActionId(Long id);
+    public Boolean existsByUserEmail(String userEmail);
+    public Boolean existsBySocialActionId(Long id);
 }
