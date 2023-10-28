f248cefb3f563ebb47dfc57ffade78c566c57fd1
PacMan111 <luizgustavossj@gmail.com>
2023-10-27 21:17:13 -0400

    feat(discovery-server): configure services to register do eureka

--- Front-end/PiraputangaDev/src/keycloak.js
+++ Front-end/PiraputangaDev/src/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 let initOptions = {
-  url: 'https://keycloak-4208.onrender.com/',
+  url: 'http://localhost:8080',
   realm: 'PiraputangaDev',
   clientId: 'frontend-client',
   onLoad: "login-required"
